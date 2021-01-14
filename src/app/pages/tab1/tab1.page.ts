d7649609f3e3ba53fbed5ca50c8ddc6497df5269
IsaacRh <isaac.roj_12@hotmail.com>
2021-01-14 17:08:10 -0600

    [CREATE/ADD] -- create button for add list and add navigation betwen screens

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { WishesService } from '../../services/wishes.service';
 import { List } from '../../models/list.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -9,7 +10,10 @@ import { List } from '../../models/list.model';
 })
 export class Tab1Page {
   
-  constructor(public wishes: WishesService) {
+  constructor(public wishes: WishesService, private router: Router) {
   }
 
+  addList(){
+    this.router.navigateByUrl('/tabs/tab1/add');
+  }
 }
